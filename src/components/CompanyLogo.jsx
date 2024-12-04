5728e0886254ae367830889ef22c9d86591e6f48
taru <taru253@gmail.com>
2024-12-04 12:57:19 +0530

    "Update CompanyLogo component: modify CSS classes and styles for container, grid, and logo elements."

--- src/components/CompanyLogo.jsx
+++ src/components/CompanyLogo.jsx
@@ -9,20 +9,20 @@ const CompanyGrid = () => {
   ];
 
   return (
-    <div className="bg-black pt-16">
+    <div className="bg-black py-10 md:py-16">
       <div className="max-w-6xl mx-auto text-center">
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-6 pl-10">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 items-center px-4 md:px-0 gap-6">
           {companies.map((company, index) => (
             <div
               key={index}
-              className="flex items-center space-x-4 p-4 rounded-lg shadow-md"
+              className="flex flex-col items-center p-4 rounded-lg hover:shadow-lg transition duration-300"
             >
               <img
                 src={company.logo}
                 alt={company.name}
-                className="w-12 h-12 object-contain"
+                className="w-16 h-16 object-contain mb-4"
               />
-              <p className="text-white text-2xl font-semibold">{company.name}</p>
+              <p className="text-white text-xl font-medium">{company.name}</p>
             </div>
           ))}
         </div>
